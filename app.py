84f76f914171ad03cd086f79d2287213ad1e7f5b
Mert Caliskan <mecaliskan95@gmail.com>
2024-10-11 13:34:30 +0300

    update

--- app.py
+++ app.py
@@ -4,7 +4,10 @@
 from PIL import Image
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r"c:\Users\mertc\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+
+# Load the invoice image
 image = Image.open("Capture2.PNG")
 text = pytesseract.image_to_string(image)
 
 print(text)
+print(type(text))
