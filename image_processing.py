a1a673d09949db5443ec126389e040c33a7a2331
Mert Caliskan <mecaliskan95@gmail.com>
2024-11-14 17:22:10 +0300

    update

--- image_processing.py
+++ image_processing.py
@@ -1,15 +1,15 @@
 import cv2
 import pytesseract
 import base64
+from skimage.filters import threshold_local
 
 class ImageProcessor:    
     @staticmethod
     def process_image(image_path: str):
         image = cv2.imread(image_path)
-        if image is None:
-            raise ValueError(f"Unable to read image: {image_path}")
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        return gray
+        T = threshold_local(gray, 21, offset=5, method="gaussian")
+        return (gray > T).astype("uint8") * 255
 
     @staticmethod
     def get_image_base64(image_path: str) -> str:
@@ -19,5 +19,7 @@ class ImageProcessor:
 
     @staticmethod
     def extract_text(image_path: str) -> str:
-        processed_image = ImageProcessor.process_image(image_path)
-        return pytesseract.image_to_string(processed_image, lang='tur+eng').upper()
+        return pytesseract.image_to_string(
+            ImageProcessor.process_image(image_path), 
+            lang='tur+eng'
+        ).upper()
